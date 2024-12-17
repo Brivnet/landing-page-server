437f44c2a2ca608423deec77f4a2bb822e122f95
Adriel Amadi <adrielamadi7@gmail.com>
2024-12-17 01:44:13 +0100

    cors-policy fixed

--- routes/router.js
+++ routes/router.js
@@ -6,6 +6,9 @@ router.use((req, res, next)=>{
     res.setHeader('Access-Control-Allow-Origin', '*')
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
     res.setHeader('Access-Control-Allow-Headers', 'Content-type, Authorization')
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
     next()
 })
 
